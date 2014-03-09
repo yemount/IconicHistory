f03fa6f03692f80457af8b0377e12e3b2ae789fb
yemount@gmail.com <shanhuan@andrew.cmu.edu>
2014-03-09 12:29:23 -0400

    favicons properly displayed. needs more filtering.

--- iconic_history.js
+++ iconic_history.js
@@ -8,13 +8,20 @@ function parseDomain(_url){
 	return matches && matches[1];
 }
 
+var m_names = new Array("Jan", "Feb", "Mar", 
+"April", "May", "June", "July", "Aug", "Sep", 
+"Oct", "Nov", "Dec");
+
+var curTimestr = "";
+
 function genFavicons(){
 	dummyA = document.createElement('a');
 	chrome.history.search({
 		'text': '',
-		'maxResults': 10000,
+		'maxResults': 100000,
 		'startTime': -1,
 	}, function (historyItems){
+		historyItems.sort(function(a, b) {return b.lastVisitTime - a.lastVisitTime});
 		console.log(historyItems.length);
 		for(var i = 0; i < historyItems.length; i++){
 			var item = historyItems[i];
@@ -27,12 +34,20 @@ function genFavicons(){
 			}
 			faviconUrl = 'chrome://favicon/' + urlMappings[domain];
 			var faviconDiv = document.createElement('div');
-			var faviconTitle = item.title.replace("\"", "");
+			var faviconTitle = item.title.replace(/\"/g, "");
 			faviconDiv.className = "faviconDiv";
+			var timestamp = new Date(item.lastVisitTime);
+			var timestr = m_names[timestamp.getMonth()] + " " + timestamp.getDate() + ", " + timestamp.getFullYear().toString().substring(2, 4);
+			if(timestr != curTimestr){
+				curTimestr = timestr;
+				var _div = document.createElement('div');
+				_div.innerHTML = timestr;
+				_div.className += "dateDiv";
+				document.getElementById('faviconHolder').appendChild(_div);
+			}
 			faviconDiv.innerHTML += '<a href="' + item.url + '"><img title="Title: ' + faviconTitle + '&#10;Visited on: ' + item.lastVisitTime + '" class="favicon" src="' + faviconUrl + '"></img></a>'
 			document.getElementById('faviconHolder').appendChild(faviconDiv);
 		}
-		console.log("all done!");
 	});
 }
 
